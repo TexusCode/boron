fe790b2cb7b1642b2200ad69cd4196690b64f075
Mahmudi Shodmehr <mahmudishodmehr@Mac-mini-Mahmudi.local>
2025-12-11 04:52:42 +0500

    updated

--- resources/views/web/partials/home-slider.blade.php
+++ resources/views/web/partials/home-slider.blade.php
@@ -12,13 +12,13 @@
 </div>
 
 
-<section class="grid grid-cols-4 gap-2 px-2 mt-4 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-12 lg:gap-8">
+<section class="grid grid-cols-4 gap-2 px-2 mt-4 sm:grid-cols-5 lg:grid-cols-6 xl:grid-cols-12 lg:gap-4">
     @foreach ($categories as $category)
-    <a href="{{ route('search', ['category' => $category->id]) }}">
-        <div class="overflow-hidden bg-red-500 rounded-lg aspect-h-1 aspect-w-1 lg:rounded-xl">
-            <img class="object-cover object-center w-full h-full duration-200 hover:scale-110" src="{{ asset('storage/'.$category->photo) }}" alt="">
+    <a href="{{ route('search', ['category' => $category->id]) }}" class="flex flex-col items-center gap-2 text-center">
+        <div class="flex items-center justify-center w-20 h-20 mx-auto overflow-hidden bg-white rounded-full shadow-sm ring-1 ring-gray-100 lg:w-24 lg:h-24">
+            <img class="object-contain w-4/5 h-4/5 duration-200 hover:scale-105" src="{{ asset('storage/'.$category->photo) }}" alt="{{ $category->name }}">
         </div>
-        <p class="mt-1 overflow-hidden text-xs leading-none text-center text-ellipsis lg:leading-none line-clamp-2">{{ $category->name }}</p>
+        <p class="w-20 text-xs text-gray-700 lg:w-24 line-clamp-2">{{ $category->name }}</p>
     </a>
     @endforeach
 </section>
